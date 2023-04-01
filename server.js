363308e393f33c2de23e35191a25ac7d6bbc5975
Dale Bendixen <dalebendixen@gmail.com>
2023-04-01 14:18:12 -0400

    set up database connection

--- server.js
+++ server.js
@@ -1,14 +1,26 @@
 const express = require('express');
+const mongoose = require('mongoose');
 const dotenv = require('dotenv');
 dotenv.config();
 const app = express();
 const port = process.env.CLUBPORT || 5000;
+const DB = process.env.MONGODB_URL;
 const GetSongsRoute = require('./routes/Radio');
 
 app.use('/api/v1/radio', GetSongsRoute);
 app.use(express.json());
-app.listen(port, () => {
-    console.log(`440club server listening on port ${port} `);
-});
 
 
+
+const startUp = async () => {
+    try {
+        mongoose.connect(DB);
+        app.listen(port, () => {
+            console.log(`440club server listening on port ${port}\nConnected to the 440club radio Database `);
+        });
+    } catch (error) {
+        console.log(error);
+    };
+};
+
+startUp();
