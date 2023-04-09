b9258a922b26d05ca2010190f9fbd8b9786d4208
Dale Bendixen <dalebendixen@gmail.com>
2023-04-09 07:22:53 -0400

    added middleware to check for null

--- controllers/RadioPlayerPlays.js
+++ controllers/RadioPlayerPlays.js
@@ -5,13 +5,14 @@ const GetRadioPlays =  async (req, res) => {
     let radioPlays = await RadioPlays.find({});
     res.status(200).send(radioPlays);
 };
-const PostRadioPlays =  async (req, res) => {
+const PostRadioPlays =  async (req, res, next ) => {
     try {
         const {id:radioPlayer} = req.params;
         const playCounts = req.body;
         if (playCounts === 'null' || playCounts === undefined) {
             return res.status(400).json({msg: "Radio Player ID Must be number"});
         }
+        next();
         const radioPlayNumber = await RadioPlays.findOneAndUpdate({_id: radioPlayer}, playCounts, {
             new:true,
             runValidators: true
