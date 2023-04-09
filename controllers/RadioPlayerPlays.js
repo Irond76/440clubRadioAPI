38ae4e6a01139cadd39c708dc12ae9aa846e6367
Dale Bendixen <dalebendixen@gmail.com>
2023-04-09 06:56:40 -0400

    added check for null

--- controllers/RadioPlayerPlays.js
+++ controllers/RadioPlayerPlays.js
@@ -9,6 +9,9 @@ const PostRadioPlays =  async (req, res) => {
     try {
         const {id:radioPlayer} = req.params;
         const playCounts = req.body;
+        if (radioPlayer === 'null') {
+            return res.status(400).json({msg: "Radio Player ID Must be number"});
+        }
         const radioPlayNumber = await RadioPlays.findOneAndUpdate({_id: radioPlayer}, playCounts, {
             new:true,
             runValidators: true
