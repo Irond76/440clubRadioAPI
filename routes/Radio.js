23524183112cda7edf54326b7dcbfc68bac6e7d4
Dale Bendixen <93141922+Irond76@users.noreply.github.com>
2024-04-19 20:21:59 -0400

    Update Radio.js

--- routes/Radio.js
+++ routes/Radio.js
@@ -1,9 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const cors = require(cors)
 const { GetSongs, PostSongs } = require('../controllers/Songs');
-
-app.use(cors())
 router.get('/', GetSongs);
 router.post('/', PostSongs);
 
